b19ce8553337bbd2ca53fed57425b41a4b302d1f
varunkumarmedam <varunkumarmedam@gmail.com>
2024-07-18 01:50:04 +0530

    Integrated with given API, updated UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,14 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { account } from '../../lib/appwrite';
 import { Router } from '@angular/router';
+import { FlightFormComponent } from "../flight-form/flight-form.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, FlightFormComponent]
 })
+
 export class HomeComponent implements OnInit {
 
   constructor(private router: Router) { }
@@ -25,8 +27,11 @@ export class HomeComponent implements OnInit {
 
   async onPageLoad(): Promise<void> {
     console.log('Home page loaded');
-    this.loggedInUser = await account.get();
-    console.log(this.loggedInUser);
+    try {
+      this.loggedInUser = await account.get();
+    } catch (error) {
+      this.router.navigate(['/']);
+    }
   }
 
   async logout() {
@@ -34,7 +39,7 @@ export class HomeComponent implements OnInit {
       await account.deleteSessions();
       this.loggedInUser = null;
     } catch (error) {
-      
+      console.log("Error while logging out, ", error)
     }
     this.router.navigate(['/']);
   }
